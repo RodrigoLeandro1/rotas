53f68cdf265d209661311186d944e0b387c4e3eb
RodrigoLeandro1 <116128994+RodrigoLeandro1@users.noreply.github.com>
2025-06-19 17:47:41 -0300

    colocando meu endereço do front do render pra se comunicar com o back

--- server.js
+++ server.js
@@ -16,12 +16,7 @@ if (!GOOGLE_MAPS_API_KEY) {
 
 // 3. CONFIGURAÇÃO DO CORS
 const allowedOrigins = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://localhost:8080',
-    'http://127.0.0.1:8080',
-    'http://127.0.0.1:5500',
-    'http://localhost:5500'
+    'https://rotas0five.onrender.com'
 ];
 
 const corsOptions = {
